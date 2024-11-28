136b836a2945f49f1a802738dd8d392f4eff00bb
Brennon Gabriel de Oliveira <brennonoliveira20014@gmail.com>
2024-11-28 01:47:07 -0300

    Correções para debug

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import { app } from './app'
 import { env } from './env'
 
-app
-  .listen({
+app.listen(
+  {
     port: env.PORT,
-  })
-  .then(() => {
-    console.log('HTTP Server Running')
-  })
+  },
+  (err, address) => {
+    if (err) {
+      console.error(err)
+      throw new Error(err.message)
+    }
+    console.log(`Server running on ${address}`)
+  },
+)
